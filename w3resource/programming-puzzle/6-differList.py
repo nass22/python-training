7c8e619226e214162a62b92c02172d14be580094
Nacer Samir <84498447+nass22@users.noreply.github.com>
2023-03-29 15:04:13 +0200

    Update 6-differList.py

--- w3resource/programming-puzzle/6-differList.py
+++ w3resource/programming-puzzle/6-differList.py
@@ -28,6 +28,13 @@ print(exercice6(y))
     SOLUTION
 """
 
+"""
+    Explanation:
+    
+    all() = Check if all items in a list are True
+    abs() = return the absolute value and remove the negative sign of a number
+"""
+
 # def test(li):
 #     return all(i in range(1000) and abs(i - j) >= 10 for i in li for j in li if i != j) and len(set(li)) == 100
 # nums = list(range(0, 1000, 10))
